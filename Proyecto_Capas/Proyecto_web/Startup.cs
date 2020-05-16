da00daa1c37c88c2881a43750fe35809461e96ce
programador <contacto@gmail.com>
2020-05-15 23:48:46 -0400

    Registrando,iniciando login

---
+++ Proyecto_Capas/Proyecto_web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_web.Startup))]
+namespace Proyecto_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
